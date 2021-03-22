eed5edb221fcf25656e2142e833630cd5b15df14
davidgrcias <davidgarciasaragih6@gmail.com>
2021-03-22 21:57:42 +0700

    done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
 function imgSlider(anything){
   document.querySelector('.starbucks').src = anything;
 }
-
-function changeCircleColor(color){
-  const circle = document.querySelector('.circle');
-    circle.style.background = color;
-}
